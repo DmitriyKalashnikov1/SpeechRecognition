b4bddf3841d163c415c88f32ddc26c77495c3061
DmitriyKalashnikov1 <slagterra2017@yandex.ru>
2022-05-22 22:42:05 +0300

    3-rd commit

--- SpeechRecognition/SpeechRecognition.py
+++ SpeechRecognition/SpeechRecognition.py
@@ -9,10 +9,7 @@ def callback(recognizer, audio):
 
     # received audio data, now we'll recognize it using Google Speech Recognition
     try:
-        # for testing purposes, we're just using the default API key
-        # to use another API key, use `r.recognize_google(audio, key="GOOGLE_SPEECH_RECOGNITION_API_KEY")`
-        # instead of `r.recognize_google(audio)`
-        tmp = recognizer.recognize_google(audio, language = 'ru-RU')
+        tmp = recognizer.recognize_google(audio, language = 'en-US')
         print("google thinks you said " + tmp)
         text += tmp
         text += '\n'
@@ -25,9 +22,6 @@ def callback(recognizer, audio):
 def listenForSeconds(sec: int):
     r = sr.Recognizer()
     m = sr.Microphone()
-  #  with m as source:
- #       r.adjust_for_ambient_noise(source)  # we only need to calibrate once, before we start listening
-    print("Start background listening")
     # start listening in the background (note that we don't have to do this inside a `with` statement)
     stop_listening = r.listen_in_background(m, callback)
     # `stop_listening` is now a function that, when called, stops background listening
@@ -39,15 +33,13 @@ def listenForSeconds(sec: int):
 
     # calling this function requests that the background listener stop listening
     stop_listening(wait_for_stop=False)
-    print("Stop background listening")
-
 
 isWorking = True
-
+SecondsForListening = 60
 
 while isWorking:
     try:
-        listenForSeconds(60)
+        listenForSeconds(SecondsForListening)
     except KeyboardInterrupt:
         isWorking = False
 
